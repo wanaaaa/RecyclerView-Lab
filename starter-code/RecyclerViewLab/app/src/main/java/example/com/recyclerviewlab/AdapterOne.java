99fef89415145953dc353038e16cb5cd280e984a
wanMac <wanaaaa@gmail.com>
2016-07-05 22:55:50 -0400

    asdf

--- starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/AdapterOne.java
+++ starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/AdapterOne.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,6 +31,15 @@ public class AdapterOne extends RecyclerView.Adapter<ViewHolderOne> {
         holder.mTextViewTitle.setText(mFirstObject.get(position).getmTitle());
         holder.mTextViewDescription.setText("This is setDescription");
         holder.mTextViewColor.setText("setColor");
+
+        View.OnClickListener onClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "I am clicked", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        holder.mButton1.setOnClickListener(onClickListener);
     }
 
     @Override
--- starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/ViewHolderOne.java
+++ starter-code/RecyclerViewLab/app/src/main/java/example/com/recyclerviewlab/ViewHolderOne.java
@@ -3,6 +3,7 @@ package example.com.recyclerviewlab;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -10,6 +11,7 @@ import android.widget.TextView;
  */
 public class ViewHolderOne extends RecyclerView.ViewHolder {
     public TextView mTextViewTitle, mTextViewDescription, mTextViewColor;
+    public Button mButton1;
 
     public ViewHolderOne (View itemView) {
         super(itemView);
@@ -17,6 +19,7 @@ public class ViewHolderOne extends RecyclerView.ViewHolder {
         mTextViewTitle = (TextView) itemView.findViewById(R.id.titleID);
         mTextViewDescription = (TextView) itemView.findViewById(R.id.descriptionID);
         mTextViewColor = (TextView) itemView.findViewById(R.id.colorID);
+        mButton1 = (Button) itemView.findViewById(R.id.button01);
 
     }
 }
